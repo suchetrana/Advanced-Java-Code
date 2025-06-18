9c8c80c40e4c2992446c23ffa4a664ccce6256c4
Suchet <suchetrana27@gmailcom>
2025-06-18 16:51:30 +0530

    Starting of Set

---
+++ src/Collections/Set/HashSetPractice.java
@@ -0,0 +1,60 @@
+package Collections.Set;
+
+import java.util.*;
+
+public class HashSetPractice {
+    public static void main(String[] args) {
+        System.out.println("Create a set");
+        Set<String> set = new HashSet<>();
+
+        System.out.println("Find size of set");
+        System.out.println("Size of set is: " + set.size());
+
+        System.out.println("Display set");
+        System.out.println("Contents of set are: " + set);
+
+        System.out.println("Add element to set");
+        set.add("Java");
+        set.add("Python");
+        set.add("C");
+        System.out.println("Size of set is: " + set.size());
+        System.out.println("Contents of set are: " + set);
+
+        System.out.println("Remove element by value from set");
+        set.remove("Python");
+        System.out.println("Size of set is: " + set.size());
+        System.out.println("Contents of set are: " + set);
+
+        System.out.println("Check if set contains element");
+        String searchableItem = "Java";
+        if (set.contains(searchableItem)) {
+            System.out.println("Set contains element: " + searchableItem);
+        }
+
+        System.out.println("Clear set");
+        set.clear();
+        System.out.println("Size of set is: " + set.size());
+        System.out.println("Contents of set are: " + set);
+
+        System.out.println("Check if set is empty");
+        if (set.isEmpty()) {
+            System.out.println("Set is empty");
+        }
+
+        System.out.println("Add elements to set");
+        List<String> list = Arrays.asList("JavaScript", "C#", "Java");
+        set.addAll(list);
+        System.out.println("Size of set is: " + set.size());
+        System.out.println("Contents of set are: " + set);
+        List<String> list2 = Arrays.asList("Python", "Java", "C");
+        set.addAll(list2);
+        System.out.println("Size of set is: " + set.size());
+        System.out.println("Contents of set are: " + set);
+
+        System.out.println("Sort elements of set");
+        List<String> list3 = new ArrayList<>(set);
+        System.out.println("Contents of list are: " + list3);
+        Collections.sort(list3);
+        System.out.println("Contents of sorted list are: " + list3);
+    }
+}
