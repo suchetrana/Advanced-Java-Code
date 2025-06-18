e7e78ce047bff3a5876100e3d4b5cd099a699971
Suchet <suchetrana27@gmailcom>
2025-06-18 15:28:59 +0530

    New Format

---
+++ src/Collections/List/ListFunctions.java
@@ -0,0 +1,39 @@
+package Collections.List;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ListFunctions {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("C#");
+        list.add("Python");
+        System.out.println("iterating using idex");
+        for(int i=0; i<list.size(); i++){
+            System.out.println("Value of element: " + list.get(i));
+        }
+        System.out.println("Using for each loop");
+        for (String s : list) {
+            System.out.println("Value of element: " + s);
+        }
+        System.out.println("Using iterator");
+        Iterator<String> it = list.iterator();
+        while(it.hasNext()){
+            System.out.println("Value of element: " + it.next());
+        }
+
+        System.out.println("List iterator -- forward"); // Advantage of listIterator is that you can move forward and backward direction also
+//       other all iterator moves in forward direction
+        ListIterator<String> li = list.listIterator();
+        while (li.hasNext()){
+            System.out.println("Value of element: " + li.next());
+        }
+        System.out.println("List iterator -- backward");
+        while(li.hasPrevious()){
+            System.out.println("Value of element: " + li.previous());
+        }
+    }
+}
---
+++ src/Collections/List/UnModifiedList.java
@@ -0,0 +1,17 @@
+package Collections.List;
+
+import java.util.List;
+
+public class UnModifiedList {
+    public static void main(String[] args) {
+        List<String> list = List.of("a", "b", "c", "d", "e", "f", "g", "h", "i");
+        System.out.println("List is unmodifiable.");
+        System.out.println("Contents of List: " + list);
+        System.out.println("List is unmodifiable.");
+        list.set(0, "x");
+        System.out.println("Addition, Removing and Clear is not allowed");
+        list.add("x");
+        list.remove("x");
+        list.clear();
+    }
+}
